650b0d671fe0c4452a1b4febe45618b88da924ae
nablabits <birrick@gmail.com>
2018-04-06 09:52:38 +0200

    Binary tests

--- tests.py
+++ tests.py
@@ -6,11 +6,36 @@ class TestUtils(unittest.TestCase):
     """Test utils."""
 
     def setUp(self):
-        utils = pnr.Utils()
+        self.utils = pnr.Utils()
+        self.numberlist = (12, 15, 25, 70)
 
-    def test_binary_gets_the_ok_number(self):
-        raise NameError('must continue here')
+    def test_binary_finds_number(self):
+        """Binary search finds the number."""
+        numberlist = self.numberlist
+        number = 25
+        result = self.utils.binary(numberlist, number)
+        self.assertTrue(result[0])
 
+    def test_binary_exclude_number(self):
+        """Binary search returns False with numbers not within list."""
+        numberlist = self.numberlist
+        number = 26
+        result = self.utils.binary(numberlist, number)
+        self.assertFalse(result[0])
+
+    def test_binary_high_number(self):
+        """Binary search returns False with numbers highers than list."""
+        numberlist = self.numberlist
+        number = 90
+        result = self.utils.binary(numberlist, number)
+        self.assertFalse(result[0])
+
+    def test_binary_lo_number(self):
+        """Binary search returns False with numbers lowers than list."""
+        numberlist = self.numberlist
+        number = 5
+        result = self.utils.binary(numberlist, number)
+        self.assertFalse(result[0])
 
 if __name__ == '__main__':
     unittest.main()
